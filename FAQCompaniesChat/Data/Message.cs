aa39f9c28f2e991671f2717caafc6b70998b228b
Marcin Stanek <marcin.stanek@elexon.co.uk>
2024-03-21 09:38:11 +0100

    chat min

---
+++ FAQCompaniesChat/Data/Message.cs
@@ -0,0 +1,13 @@
+namespace FAQCompaniesChat.Data;
+public class Message
+{
+    public DateTime TimeStamp { get; set; }
+    public string? Body { get; set; }
+    public bool IsRequest { get; set; }
+    public Message(string body, bool isRequest)
+    {
+        TimeStamp = DateTime.Now;
+        Body = body;
+        IsRequest = isRequest;
+    }
+}
